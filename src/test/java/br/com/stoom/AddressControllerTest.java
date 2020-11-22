88ee9b1fb6d6f1e45ad26944047bf9618ab4f473
livaniosilva <livanio.silva2@gmail.com>
2020-11-22 14:19:57 -0300

    test class

--- src/test/java/br/com/stoom/AddressControllerTest.java
+++ src/test/java/br/com/stoom/AddressControllerTest.java
@@ -1,9 +1,22 @@
 package br.com.stoom;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import br.com.stoom.controller.AddressController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
 public class AddressControllerTest {
+
+    @Autowired
+    private AddressController controller;
+
+    @Test
+    public void test() throws Exception{
+        assertThat(controller).isNotNull();
+    }
 }
